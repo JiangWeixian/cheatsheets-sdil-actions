921899351670cf920339238398ec47f5c6c3b56e
jiangWeixian <jiangweixian1994@gmail.com>
2021-01-17 13:53:28 +0800

    🔧 input schema

--- dist/index.js
+++ dist/index.js
@@ -24,14 +24,17 @@ async function run() {
     const url = process.env.SLACK_WEBHOOK
     const host = core.getInput('CHEATSHEET_HOST')
     const channel = core.getInput('SLACK_CHANNEL')
+    const debug = core.getInput('debug') || false
+    debug && core.info(`https://${host}/api/someday`)
     const webhook = new IncomingWebhook(url);
     const response = await fetch(`https://${host}/api/someday`, { method: 'GET' }).then(res => res.json())
-    // core.info(JSON.stringify(response))
+    debug && core.info(JSON.stringify(response))
+    debug && core.info(JSON.stringify(channel))
     if (response && response[0]) {
       const { body, created_at, labels, title } = response[0]
       await webhook.send({
         username: 'Cheatsheet Someday',
-        channel: channel,
+        channel: '#til',
         icon_url: 'https://a.slack-edge.com/production-standard-emoji-assets/13.0/apple-large/1f9d1-1f3fb-200d-1f4bb@2x.png',
         attachments: [
           {
