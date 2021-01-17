72bb77cb1eced2f454c431ab369659f3c34cec17
jiangWeixian <jiangweixian1994@gmail.com>
2021-01-17 13:46:45 +0800

    ✨ support custom slack channel

--- index.js
+++ index.js
@@ -7,13 +7,16 @@ const { IncomingWebhook } = require('@slack/webhook')
 async function run() {
   try {
     const url = process.env.SLACK_WEBHOOK
+    const host = core.getInput('CHEATSHEET_HOST')
+    const channel = core.getInput('SLACK_CHANNEL')
     const webhook = new IncomingWebhook(url);
-    const response = await fetch('https://jiangweixian-cheatsheets.vercel.app/api/someday', { method: 'GET' }).then(res => res.json())
+    const response = await fetch(`https://${host}/api/someday`, { method: 'GET' }).then(res => res.json())
     // core.info(JSON.stringify(response))
     if (response && response[0]) {
       const { body, created_at, labels, title } = response[0]
       await webhook.send({
         username: 'Cheatsheet Someday',
+        channel: channel,
         icon_url: 'https://a.slack-edge.com/production-standard-emoji-assets/13.0/apple-large/1f9d1-1f3fb-200d-1f4bb@2x.png',
         attachments: [
           {
