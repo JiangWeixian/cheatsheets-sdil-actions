52bebc43e1148cd7372c79621453b33691ef01ea
jiangWeixian <jiangweixian1994@gmail.com>
2021-01-17 09:55:07 +0800

    ✨ send issue body

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const wait = require('./wait');
 const fetch = require('node-fetch')
+const dayjs = require('dayjs')
 const { IncomingWebhook } = require('@slack/webhook')
 
 // most @actions toolkit packages have async methods
@@ -12,10 +13,31 @@ async function run() {
     core.info(`Waiting ${ms} milliseconds ...`);
     const response = await fetch('https://jiangweixian-cheatsheets.vercel.app/api/someday', { method: 'GET' }).then(res => res.json())
     // core.info(JSON.stringify(response))
-    core.info(url)
-    await webhook.send({
-      text: 'I\'ve got news for you...',
-    });
+    if (response && response[0]) {
+      const { body, created_at, labels } = response[0]
+      await webhook.send({
+        username: 'Cheatsheet Someday',
+        attachments: [
+          {
+            fallback: body,
+            pretext: body,
+            text: body,
+            fields: [
+              {
+                title: 'Labels',
+                value: labels.map(label => label.name).join(','),
+                short: false,
+              },
+              {
+                title: 'Learned At',
+                value: dayjs(created_at).format('YYYY-MM-DD HH:mm:ss'),
+                short: false,
+              }
+            ]
+          }
+        ]
+      });
+    }
     core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
     await wait(parseInt(ms));
     core.info((new Date()).toTimeString());
